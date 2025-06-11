e44a7f27278b5b5594d5b592278fe1c5a6ce4450
Arap-Tum <tumkibet32@gmail.com>
2025-06-11 08:28:00 +0300

    fix: update file size limit in multer configuration from 1mb to 2mb

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 2 * 1024 * 1024 }, //1mb limit
+  limits: { fileSize: 2 * 1024 * 1024 }, //2mb limit
   fileFilter: (_req, file, cb) => {
     if (file.mimetype.startsWith("image/")) {
       cb(null, true);
